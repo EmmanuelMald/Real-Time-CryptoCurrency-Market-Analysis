a00bb57e61e14660632381b6137e82149fca90b3
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-06-10 00:35:44 -0600

    Add a function to publish a message in the pubsub script

--- streaming_pipeline/utils/gcp/pubsub.py
+++ streaming_pipeline/utils/gcp/pubsub.py
@@ -1,5 +1,7 @@
 from google.cloud import pubsub_v1
 from loguru import logger
+from concurrent import futures
+from typing import Callable, Optional
 
 # Create a publisher client for Google Cloud Pub/Sub
 publisher = pubsub_v1.PublisherClient()
@@ -95,3 +97,38 @@ def delete_topic(project_id: str, topic_name: str) -> None:
     logger.info(
         f"Pub/Sub topic '{topic_name}' deleted successfully from project '{project_id}'."
     )
+
+
+def publish_message(
+    project_id: str,
+    topic_name: str,
+    message: str,
+    callback: Optional[Callable[[futures.Future], None]] = None,
+) -> None:
+    """
+    Publish a message to a Pub/Sub topic in the specified GCP project.
+    The code was adapted from: https://cloud.google.com/pubsub/docs/publisher
+
+    Args:
+        project_id (str): The GCP project ID where the topic is located.
+        topic_name (str): The name of the Pub/Sub topic to publish to.
+        message (str): The message to publish.
+        callback (Callable[[futures.Future], None], optional): A callback function to handle the result of the publish operation.
+    """
+    # Validate input parameters
+    if not all(
+        [isinstance(x, str) and x != "" for x in [project_id, topic_name, message]]
+    ):
+        raise ValueError(
+            "project_id, topic_name, and message must be non-empty strings."
+        )
+
+    # Generate the path for the topic
+    topic_path = publisher.topic_path(project_id, topic_name)
+
+    # Publish the message
+    publish_future = publisher.publish(topic_path, data=message.encode("utf-8"))
+    logger.info(f"Publishing message to Pub/Sub topic {topic_name}")
+
+    if callback:
+        publish_future.add_done_callback(callback)
