70fd5c1434eef68a6996b8bf351c37bcff0267cb
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-06-12 01:32:41 -0600

    Creation of a setup.py file that will tell dataflow how to build the project, due to it contains custom packages

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-import setuptools
+from setuptools import setup
 
-# Read dependencies from requirements.txt,
-# requirements.txt is created when executing the make command
-with open("requirements.txt") as f:
-    install_requires = f.read().splitlines()
-
-setuptools.setup(
-    name="streaming-pipeline-crypto",
-    version="1.0",
-    install_requires=install_requires,  # Use the list read from requirements.txt
-    packages=setuptools.find_packages(),
-)
+setup()
